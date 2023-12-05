544cb074d45ff3a0d32b37c518f018d1900c4214
HikaruHK <isabelle.50@hotmail.com>
2023-12-05 16:12:17 -0300

    Main
    Main body edited

--- DIW Project/js/script.js
+++ DIW Project/js/script.js
@@ -4,10 +4,10 @@ function toggleMenu() {
     
     if (submenu.style.display === 'block') {
         // Se o submenu está visível, ajusta a posição do conteúdo principal
-        mainContent.style.marginTop = '85px'; // Ajuste conforme necessário
+        mainContent.style.marginTop = '127px'; // Ajuste conforme necessário
     } else {
         // Se o submenu está oculto, restaura a posição padrão do conteúdo principal
-        mainContent.style.marginTop = '0';
+        mainContent.style.marginTop = '67px';
     }
 }
 
